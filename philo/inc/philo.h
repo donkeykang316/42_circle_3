65442a4deae101f12ebd4e8b302b01d0da308b30
donkeykang316 <kang_an_an@hotmail.de>
2024-02-09 00:43:57 +0100

    learning thread

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:48:32 by kaan              #+#    #+#             */
-/*   Updated: 2024/02/08 17:51:06 by kaan             ###   ########.fr       */
+/*   Updated: 2024/02/08 23:23:55 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,13 +6,37 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:42:02 by kaan              #+#    #+#             */
-/*   Updated: 2024/02/08 17:52:34 by kaan             ###   ########.fr       */
+/*   Updated: 2024/02/09 00:43:28 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
+void	*eat_food(void *data)
+{
+	printf("Philosopher get ready\n");
+	sleep(2);
+	printf("Philosopher is eating\n");
+	return (NULL);
+}
+
 int	main(int ac, char **av)
 {
-	printf("Hello Kang!\n");
+	pthread_t	philo[5];
+	int			i;
+	int			x;
+	int			y;
+
+	i = 0;
+	while (i != 5)
+	{
+		pthread_create(&philo[i], NULL, eat_food, NULL);
+		i++;
+	}
+	i = 0;
+	while (i != 5)
+	{
+		pthread_join(philo[i], NULL);
+		i++;
+	}
 }
