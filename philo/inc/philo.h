fa266332915c967e6c34435a4f20d7d234dcdf35
donkeykang316 <kang_an_an@hotmail.de>
2024-02-18 13:24:19 +0100

    step forward

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:48:32 by kaan              #+#    #+#             */
-/*   Updated: 2024/02/15 23:22:14 by kaan             ###   ########.fr       */
+/*   Updated: 2024/02/18 11:26:34 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,25 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-typedef struct s_monitor t_monitor;
-
 typedef struct s_philo
 {
+	long				philo_nbr;
+	long				time_to_die;
+	long				time_to_eat;
+	long				time_to_sleep;
+	long				eat_times;
 	int					id;
 	int					dead;
 	pthread_t			ph;
 	pthread_mutex_t		fork_l;
 	pthread_mutex_t		fork_r;
 	long				last_meal_time;
-	struct s_monitor	*monitor;
 }	t_philo;
 
-struct s_monitor
+typedef struct s_monitor 
 {
-	long	philo_nbr;
-	long	time_to_die;
-	long	time_to_eat;
-	long	time_to_sleep;
-	long	eat_times;
 	t_philo	*philo;
-};
+}	t_monitor;
 
 
 void	*safe_malloc(size_t byte);
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,66 +6,72 @@
 /*   By: kaan <kaan@student.42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:42:02 by kaan              #+#    #+#             */
-/*   Updated: 2024/02/15 23:27:31 by kaan             ###   ########.fr       */
+/*   Updated: 2024/02/18 13:22:33 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	input_parse(t_monitor *monitor, char **av)
+void	input_parse(t_philo *philo, char **av)
 {
-	monitor->philo_nbr = ft_atol(av[1]);
-	monitor->time_to_die = ft_atol(av[2]);
-	monitor->time_to_eat = ft_atol(av[3]);
-	monitor->time_to_sleep = ft_atol(av[4]);
+	philo->philo_nbr = ft_atol(av[1]);
+	philo->time_to_die = ft_atol(av[2]);
+	philo->time_to_eat = ft_atol(av[3]);
+	philo->time_to_sleep = ft_atol(av[4]);
 	if (av[5])
-		monitor->eat_times = ft_atol(av[5]);
+		philo->eat_times = ft_atol(av[5]);
 }
 
-void	monitor_init(t_monitor *monitor)
+void	monitor_init(t_monitor *monitor, t_philo philo, char **av)
 {
-	t_philo			*philo;
-	struct timeval	time;
+	monitor->philo = &philo;
+}
 
-	monitor->philo = safe_malloc(sizeof(t_philo) * monitor->philo_nbr);
-	philo = safe_malloc(sizeof(t_philo));
-	philo->monitor = monitor;
-	monitor->philo = philo;
-	philo->id = 0;
-	philo->dead = 0;
-	pthread_mutex_init(&(philo->fork_l), NULL);
-	pthread_mutex_init(&(philo->fork_r), NULL);
+void	philo_init(t_philo *philo, t_monitor *monitor, char **av)
+{
+	struct timeval	time;
+	int				i;
+
 	gettimeofday(&time, NULL);
-	philo->last_meal_time = time.tv_usec;
+	monitor->philo = safe_malloc(sizeof(t_philo) * ft_atol(av[1]));
+	i = 0;
+	while (i < ft_atol(av[1]))
+	{
+		input_parse(philo, av);
+		philo->id = i + 1;
+		philo->dead = 0;
+		pthread_mutex_init(&(philo->fork_l), NULL);
+		pthread_mutex_init(&(philo->fork_r), NULL);
+		philo->last_meal_time = time.tv_usec;
+		monitor->philo[i] = *philo;
+		i++;
+	}
 }
 
 void	*action(void *data)
 {
-	t_philo	*philo;
+	t_philo			*philo;
 
 	philo = (t_philo *)data;
-	printf("error:%ld\n", philo->monitor->time_to_eat);
 	printf("Philosopher%d eats\n", philo->id);
-	//usleep(philo->monitor->time_to_eat);
+	usleep(philo->time_to_eat);
 	printf("Philosopher%d finishs\n", philo->id);
 	return (NULL);
 }
 
-void	simulation(t_monitor *monitor)
+void	simulation(t_philo philo, t_monitor *monitor)
 {
 	int			i;
 
 	i = 0;
-	//pass two structures to each other possible?
-	while (i < monitor->philo_nbr)
+	while (i < philo.philo_nbr)
 	{
-		monitor->philo[i].id = i;
-		if (pthread_create(&(monitor->philo[i].ph), NULL, action, &(monitor->philo[i])))
+		if (pthread_create(&(monitor->philo[i].ph), NULL, &action, &(monitor->philo[i])))
 			return ;
 		i++;
 	}
 	i = 0;
-	while (i < monitor->philo_nbr)
+	while (i < philo.philo_nbr)
 	{
 		if (pthread_join((monitor->philo[i].ph), NULL))
 			return ;
@@ -75,15 +81,17 @@ void	simulation(t_monitor *monitor)
 
 int	main(int ac, char **av)
 {
-	t_monitor	*monitor;
+	t_philo		*philo;
+	t_monitor	monitor;
 
 	if (ac == 5 || ac == 6)
 	{
-		monitor = safe_malloc(sizeof(t_monitor));
-		input_parse(monitor, av);
-		monitor_init(monitor);
-		simulation(monitor);
-		free(monitor);
+		philo = safe_malloc(sizeof(t_philo));
+		monitor_init(&monitor, *philo, av);
+		philo_init(philo, &monitor, av);
+		simulation(*philo, &monitor);
+		free(monitor.philo);
+		free(philo);
 	}
 	else
 		printf("Wrong Arguments!\n");
